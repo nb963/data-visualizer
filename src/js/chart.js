733cbbe736798cd1d592e3018b609c10662e8664
nb963 <2003nitya@gmail.com>
2023-12-22 20:26:56 +0400

    working data for given data + arithmetic

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,20 +1,59 @@
-// Ensure the Chart class is loaded in the global context
-// import Chart from 'chart.js/auto/'
+import data from './data/rawreturns.json' assert { type: 'json' }
 
-const xVals = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
-const yVals = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+let xVals = []
+
+data.forEach(function (item) {
+    // Get the keys of the object and then get the value of the 
+    let keys = Object.keys(item)
+    xVals.push(item[keys[1]])
+});
+
+
+let yVals1Percent = []
+
+data.map(function (item) {
+    // Get the keys of the object and then get the value of the 
+    let keys = Object.keys(item);
+    let add1percent = 1 + (item[keys[2]] / 100)
+    yVals1Percent.push(add1percent)
+    return add1percent;
+});
+
+let yVals = [];
+let currProduct = 1;
+
+yVals1Percent.forEach(function (currentValue) {
+    currProduct *= currentValue;
+    yVals.push(parseFloat((currProduct - 1) * 100).toFixed(4));
+});
+
+console.log(yVals);
+
 
 let currChart = document.getElementById('myChart').getContext('2d')
 
+Chart.defaults.font.size = 16;
+Chart.defaults.font.family = "serif"
 let lineChart = new Chart(currChart, {
     type: 'line',
     data: {
         labels: xVals,
         datasets: [{
             data: yVals,
-            backgroundColor: "rgba(0,0,255,1.0)",
-            borderColor: "rgba(0,0,255,0.1)",
+            borderColor: '#083966',
+            backgroundColor: '#516c8a',
         }]
     },
-    options: {},
+    options: {
+        scales: {
+            x: {
+                position: 'bottom',
+            },
+            y: {
+                beginAtZero: false,
+            },
+        },
+    },
 })
+
+// function zoomClick()
